68998c6746b3d324a9070ceda20bd4e574cf3271
Atul <atulsourabh083@gmail.com>
2024-02-20 16:59:00 +0530

    Hero Banner done

--- src/App.jsx
+++ src/App.jsx
@@ -19,22 +19,30 @@ function App() {
 
   const dispatch = useDispatch()
   const {url} = useSelector((state) => state.home)
+  console.log(url)
 
   useEffect(() => {
-    apiTesting()
+    fetchApiConfig()
   }, [])
 
-  const apiTesting = () => {
-      fetchDataFromApi("/movie/popular").then((res) => {
+  const fetchApiConfig = () => {
+      fetchDataFromApi("/configuration").then((res) => {
          console.log(res) 
-         dispatch(getApiConfiguration(res))
+
+         const url = {
+            backdrop: res.images.secure_base_url + "original",
+            poster: res.images.secure_base_url + "original",
+            profile: res.images.secure_base_url + "original",
+         };
+
+         dispatch(getApiConfiguration(url))
       
       })
   }
 
   return (
     <BrowserRouter>
-      <Header />
+      {/* <Header /> */}
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/:mediaType/:id" element={<Details />} />
@@ -42,7 +50,7 @@ function App() {
           <Route path="/explore/:mediaType" element={<Explore />} />
           <Route path="*" element={<PageNotFound />} />
         </Routes>
-      <Footer />
+      {/* <Footer /> */}
     </BrowserRouter>
   )
 }
